a0c34b2e60ee8b0a638dd6031ef574c38743c236
Pedro Meroño Auñón <pmeronoaunon@gmail.com>
2025-07-31 01:31:51 +0200

    Agregada informacion sobre eslint

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -12,7 +12,11 @@ return {
         require("none-ls.diagnostics.eslint_d"),
       },
     })
-
+    -- para utilizar eslint se debe inicializar eslint en el proyecto angular
+    -- con `npm init @eslint/config@latest` y luego en el package.json se debe introducir:
+    --  "eslintConfig": {
+    --    "extends": "eslint:recommended"
+    --  }
     vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
   end,
 }
